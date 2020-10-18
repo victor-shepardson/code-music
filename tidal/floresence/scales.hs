56c40992a1410e6f26fc2b8c9939837308ad3be5
Victor Shepardson <victor.shepardson@gmail.com>
2020-10-18 19:12:09 -0400

    move from 05-29 to floresence project

---
+++ tidal/floresence/scales.hs
@@ -0,0 +1,11 @@
+just2semi j = 12 * (log j) / (log 2)
+semi2just s = exp (s * (log 2) / 12)
+newscales = [
+      ("justionian",     map just2semi [1,   9/8,   5/4,   4/3,   3/2,   5/3, 15/8]),
+      ("justdorian",     map just2semi [1,   9/8,   6/5,   4/3,   3/2,   5/3, 16/9]),
+      ("justphrygian",   map just2semi [1, 16/15,   6/5,   4/3,   3/2,   8/5, 16/9]),
+      ("justlydian",     map just2semi [1,   9/8,   5/4, 45/32,   3/2,   5/3, 15/8]),
+      ("justmixolydian", map just2semi [1,   9/8,   5/4,   4/3,   3/2,   5/3, 16/9]),
+      ("justaeolian",    map just2semi [1,   9/8,   6/5,   4/3,   3/2,   8/5, 16/9]),
+      ("justlocrian",    map just2semi [1, 16/15,   6/5,   4/3,   45/32, 8/5, 16/9]),
+      ("tenthirdsdorian",map just2semi [1, 1.11572158, 1.2, 1.3388659, 1.49380158, 1.66666667, 1.7925619])] -- third root of 10/3 tuning
